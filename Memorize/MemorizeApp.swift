d53f92a3825f32a613238c91c4f5dcb3d3ac032d
acs027 <alicihansarac@gmail.com>
2024-10-04 05:09:56 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by ali cihan on 4.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
